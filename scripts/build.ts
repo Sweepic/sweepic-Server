e72922b54978efc0ece90a08a61da56e1a97a539
jjiinaaa <qka0103@gmail.com>
2025-01-08 10:40:41 +0900

    [SWEP-28] GTS(Google Typescript Style) 연결결

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,13 @@
-import esbuild from "esbuild";
+import esbuild from 'esbuild';
 
 await esbuild.build({
-  entryPoints: ["./src/app.ts"],
+  entryPoints: ['./src/app.ts'],
   bundle: true,
-  format: "cjs",
-  outdir: "./dist",
-  outExtension: { ".js": ".cjs" },
-  platform: "node",
-  target: "node20",
+  format: 'cjs',
+  outdir: './dist',
+  outExtension: {'.js': '.cjs'},
+  platform: 'node',
+  target: 'node20',
   minify: true,
   keepNames: true,
 });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import dotenv from "dotenv";
-import cors from "cors";
-import express, { Request, Response, Express, NextFunction } from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express, {Request, Response, Express, NextFunction} from 'express';
 
 dotenv.config();
 
@@ -8,12 +8,12 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
-app.get("/", (req: Request, res: Response): void => {
-  res.send("Sweepic");
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Sweepic');
 });
 
 app.listen(port, () => {
