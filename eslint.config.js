39883ef614f7b9abc1a6f8b7265339918919af96
JinHa Park <160022452+jjiinaaa@users.noreply.github.com>
2025-01-09 17:32:55 +0900

    [SWEP-28] GTS 적용 및 코드 컨벤션 문서 정리 (#18)
    * [SWEP-28] GTS(Google Typescript Style) 연결결
    
    * [SWEP-26] 리드미 파일 추가
    
    * [SWEP-28] GTS 번역본 등록
    
    * [SWEP-28] lint 실행 에러 해결결
    
    * [SWEP-28] lint 에러 재해결
    
    * [SWEP-28] yarn.cmd 수정정
    
    * [SWEP-28] lint, fix 시 parser 에러 해결
    
    * [SWEP-28] 컴파일 결과물 파일 미생성

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser, // TypeScript 파서
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        //project: './tsconfig.json', // tsconfig.json 경로 추가
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin, // 플러그인을 객체로 추가
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-namespace': 'off', // namespace 허용
+    },
+  },
+];
