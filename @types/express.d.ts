d97f6c223a968fed8b16f289465e9b4d86d23d51
jonaeunnn <skdms123457@gamil.com>
2025-01-27 14:38:59 +0900

    [SWEP-61] develop 브런치 병합

--- @types/express.d.ts
+++ @types/express.d.ts
@@ -5,12 +5,12 @@ declare global {
     export interface User extends UserModel {}
 
     export interface MulterS3File extends Multer.File {
-      key: string;
+      key: string; // multer-s3에서 추가된 location 속성
     }
 
     export interface Request {
-      uploadDirectory: bigint;
-      file?: MulterS3File;
+      uploadDirectory: bigint; // 업로드된 디렉토리 정보
+      file?: MulterS3File; // MulterS3File 타입으로 확장
     }
 
     export interface Response {
