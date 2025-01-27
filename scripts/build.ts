5dac6febce25100e67b7458a9423893402292d4c
GodUser1005 <jijone1005@naver.com>
2025-01-27 19:41:44 +0900

    [SWEP-60] build.ts console.log 삭제

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,32 +2,27 @@ import esbuild from 'esbuild';
 import {copy} from 'esbuild-plugin-copy';
 
 async function build() {
-  await esbuild
-    .build({
-      entryPoints: ['./src/app.ts'],
-      bundle: true,
-      format: 'cjs',
-      outdir: './dist',
-      outExtension: {'.js': '.cjs'},
-      platform: 'node',
-      target: 'node20',
-      minify: false,
-      keepNames: true,
-      metafile: true,
-      plugins: [
-        copy({
-          assets: [
-            {
-              from: ['./node_modules/swagger-ui-dist/*'],
-              to: ['./'],
-            },
-          ],
-        }),
-      ],
-    })
-    .then(result => {
-      console.log(result.metafile); // 번들에 포함된 파일들 확인
-    });
+  await esbuild.build({
+    entryPoints: ['./src/app.ts'],
+    bundle: true,
+    format: 'cjs',
+    outdir: './dist',
+    outExtension: {'.js': '.cjs'},
+    platform: 'node',
+    target: 'node20',
+    minify: false,
+    keepNames: true,
+    plugins: [
+      copy({
+        assets: [
+          {
+            from: ['./node_modules/swagger-ui-dist/*'],
+            to: ['./'],
+          },
+        ],
+      }),
+    ],
+  });
 }
 
 build();
