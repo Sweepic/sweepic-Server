a6ffcd8f08a99b2dc993d76007d1fdd52ce2ee63
jjiinaaa <qka0103@gmail.com>
2025-02-05 12:01:28 +0900

    [SWEP-53] 태그 생성 시 mediaId 찾기 로직 제외

--- src/services/tag.service.ts
+++ src/services/tag.service.ts
@@ -1,21 +1,15 @@
 import {responseFromImageTag} from '../dtos/tag.dto.js';
 import {BodyToTag, ResponseFromImageTag} from '../models/tag.model.js';
-import {
-  addImageTag,
-  getImageTag,
-  getImage,
-} from '../repositories/tag.repositories.js';
+import {addImageTag, getImageTag} from '../repositories/tag.repositories.js';
 
 /**
  * 태그 생성
  * 생성 시, 태그가 존재하면 어떤 태그인지 알려주고, 존재하지 않으면 생성
  */
 async function tagCreate({
-  mediaId,
+  imageId,
   tags,
 }: BodyToTag): Promise<ResponseFromImageTag[]> {
-  const imageId = await getImage(mediaId);
-
   await addImageTag({imageId, tags});
 
   const imageTagData = await getImageTag(imageId);
