dc83d3d96ba32bb52fe669b0867d790f4fadc958
socializedistp <seolyein616@gmail.com>
2025-02-09 21:08:04 +0900

    [SWEP-58] mypage 필요없는 라우터 삭제

--- src/routers/mypage.routers.ts
+++ src/routers/mypage.routers.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 export const myPageRouter = express.Router();
-import { updateUserNameController, updateUserGoalCountController } from '../controllers/user.controller.js';
 import {getUser, deleteUser} from '../controllers/user.mypage.controllers.js';
 
 myPageRouter.get('/', getUser);
-myPageRouter.patch('/name', updateUserNameController);
-myPageRouter.patch('/goal', updateUserGoalCountController);
 myPageRouter.delete('/', deleteUser);
