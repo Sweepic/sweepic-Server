edb6c4d0e65828e099d36068f621dc48d80c67b8
asjasj3964 <84120715+asjasj3964@users.noreply.github.com>
2025-02-10 22:51:45 +0900

    [SWEP-89] 메모 텍스트 저장 API tsoa 적용 및 여러 multer 객체 사용할 수 있도록 구현 (#189)

--- src/controllers/trust.controller.ts
+++ src/controllers/trust.controller.ts
@@ -1,61 +1,85 @@
 import {Request, Response, NextFunction} from 'express';
 import * as trustService from '../services/trust.service.js';
-import {SearchNoResultsError,ServerError,DataValidationError} from 'src/errors.js';
+import {SearchNoResultsError, DataValidationError} from 'src/errors.js';
 import {StatusCodes} from 'http-status-codes';
 
-export const handleImageStatus = async (req: Request, res: Response, next:NextFunction): Promise<void> => {
-    try {
-        const {mediaId} = req.body;
-        const parsedMediaId = parseInt(mediaId);
-        if (isNaN(parsedMediaId)) {
-            throw new SearchNoResultsError({searchKeyword: 'mediaId가 올바르지 않습니다'});
-                }
-        const updatedImage = await trustService.deactivateImages(mediaId);
-        if (!updatedImage) {
-            throw new SearchNoResultsError({ searchKeyword: '해당 mediaId에 대한 이미지가 존재하지 않습니다' });
-        }
-        const result = {
-            mediaId: updatedImage.mediaId,
-            status: updatedImage.status
-        };
-        res.status(StatusCodes.OK).success(result);
-    } catch (error) {
-        next(error);
+export const handleImageStatus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const {mediaId} = req.body;
+    const parsedMediaId = parseInt(mediaId);
+    if (isNaN(parsedMediaId)) {
+      throw new SearchNoResultsError({
+        searchKeyword: 'mediaId가 올바르지 않습니다',
+      });
     }
+    const updatedImage = await trustService.deactivateImages(mediaId);
+    if (!updatedImage) {
+      throw new SearchNoResultsError({
+        searchKeyword: '해당 mediaId에 대한 이미지가 존재하지 않습니다',
+      });
+    }
+    const result = {
+      mediaId: updatedImage.mediaId,
+      status: updatedImage.status,
+    };
+    res.status(StatusCodes.OK).success(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const handleImageRestore = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const {mediaIds} = req.body;
-        if (!Array.isArray(mediaIds)) {
-            throw new SearchNoResultsError({searchKeyword: 'mediaIds가 올바르지 않습니다'});
-        }
-        const restoredImages = await trustService.restoreImages(mediaIds);
-        if (!restoredImages.length) {
-            throw new SearchNoResultsError({ searchKeyword: '해당 mediaIds에 대한 이미지가 존재하지 않습니다' });
-        }
-        const result = restoredImages.map(image => ({
-            mediaId: image.mediaId,
-            status: image.status
-        }));
-        res.status(StatusCodes.OK).success(result);
-    } catch (error) {
-        next(error);
+export const handleImageRestore = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const {mediaIds} = req.body;
+    if (!Array.isArray(mediaIds)) {
+      throw new SearchNoResultsError({
+        searchKeyword: 'mediaIds가 올바르지 않습니다',
+      });
     }
+    const restoredImages = await trustService.restoreImages(mediaIds);
+    if (!restoredImages.length) {
+      throw new SearchNoResultsError({
+        searchKeyword: '해당 mediaIds에 대한 이미지가 존재하지 않습니다',
+      });
+    }
+    const result = restoredImages.map(image => ({
+      mediaId: image.mediaId,
+      status: image.status,
+    }));
+    res.status(StatusCodes.OK).success(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const handleImageDelete = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const { mediaIds } = req.body;
-        if (!Array.isArray(mediaIds)) {
-            throw new SearchNoResultsError({searchKeyword: 'mediaIds가 올바르지 않습니다'});
-        }
-        const deleteable = await trustService.deleteImages(mediaIds);
-        if(!deleteable) {
-            throw new DataValidationError({reason: '해당 사진이 휴지통에 존재하지 않습니다'});
-        }
-        res.status(StatusCodes.OK).success(deleteable);
-    } catch (error) {
-        next(error);
+export const handleImageDelete = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const {mediaIds} = req.body;
+    if (!Array.isArray(mediaIds)) {
+      throw new SearchNoResultsError({
+        searchKeyword: 'mediaIds가 올바르지 않습니다',
+      });
     }
+    const deleteable = await trustService.deleteImages(mediaIds);
+    if (!deleteable) {
+      throw new DataValidationError({
+        reason: '해당 사진이 휴지통에 존재하지 않습니다',
+      });
+    }
+    res.status(StatusCodes.OK).success(deleteable);
+  } catch (error) {
+    next(error);
+  }
 };
