d2bbd3ccf551282f6ad56ebef91d11918683724b
JinHa Park <160022452+jjiinaaa@users.noreply.github.com>
2025-02-10 12:42:52 +0900

    hotfilx: 오타 수정

--- src/routers/mypage.routers.ts
+++ src/routers/mypage.routers.ts
@@ -2,6 +2,6 @@ import express from 'express';
 export const myPageRouter = express.Router();
 import {getUser, logOutUser, deleteUser} from '../controllers/user.mypage.controllers.js';
 
-myPageRouter.get('/', getUser
+myPageRouter.get('/', getUser);
 myPageRouter.get('/logout', logOutUser);
 myPageRouter.patch('/', deleteUser);
