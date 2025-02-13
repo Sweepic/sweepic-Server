aff370bce1d4d45e4415321aa2a2a30b5a73f2bf
asjasj3964 <asjasj3964@naver.com>
2025-02-13 11:52:46 +0900

    [SWEP-96] 폴더 생성 및 사진 저장 API 주석처리 + multer 미들웨어 실행 전에 폴더 ID 검사

--- src/s3/image.uploader.middleware.ts
+++ src/s3/image.uploader.middleware.ts
@@ -1,12 +1,32 @@
 import {Request, Response, NextFunction} from 'express';
 import {imageUploader} from './image.uploader.js';
-// import {PhotoValidationError} from '../errors.js';
-
-export const ImageUploadMiddleware = (
+import {getMemoFolder} from 'src/repositories/memo-folder.repository.tsoa.js';
+import {DataValidationError, FolderNotFoundError} from 'src/errors.js';
+import console from 'console';
+export const ImageUploadMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
+  const userId = BigInt(req.user!.id);
+  if (req.params === null) {
+    return next(
+      new DataValidationError({reason: 'folderId가 유효하지 않습니다.'}),
+    );
+  }
+  try {
+    const folderId = req.params.folderId;
+    console.log(req.params);
+    const checkFolder = await getMemoFolder(BigInt(folderId));
+
+    if (!checkFolder || checkFolder.userId !== userId) {
+      return next(new FolderNotFoundError({folderId: BigInt(folderId)}));
+    }
+    const uploadDirectory = folderId;
+    req.uploadDirectory = BigInt(uploadDirectory);
+  } catch (error) {
+    return next(error);
+  }
   imageUploader.single('image')(req, res, err => {
     if (err) {
       next(err);
